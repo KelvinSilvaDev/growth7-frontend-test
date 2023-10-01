98649d59b412d2316c252e3def452aafecc73416
Kelvin Oliveira <63024154+KelvinSilvaDev@users.noreply.github.com>
2023-10-01 01:17:20 -0300

    fix: text colors

--- src/components/JobLists/index.tsx
+++ src/components/JobLists/index.tsx
@@ -7,37 +7,38 @@ const JobList = ({ jobs }: any) => {
     console.log(jobs)
     return (
         <>
-        {jobs.map((jobSet: any, setIndex: number) => (
-  <div key={setIndex}> {/* Use um índice único para a chave da div */}
-    <Typography variant="h6" gutterBottom>
-      Grupo {setIndex + 1}
-    </Typography>
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>ID</TableCell>
-          <TableCell>Descrição</TableCell>
-          <TableCell>Data Máxima de Conclusão</TableCell>
-          <TableCell>Tempo Estimado</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {jobSet.map((job: any, jobIndex: number) => (
-          <TableRow key={`${job.ID}-${setIndex}-${jobIndex}`}>
-            <TableCell>
-              <Typography variant="body1" component="p">
-                {job.ID}
-              </Typography>
-            </TableCell>
-            <TableCell>{job.Descrição}</TableCell>
-            <TableCell>{job['Data Máxima de conclusão']}</TableCell>
-            <TableCell>{job['Tempo estimado']}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </div>
-))}
+            {jobs.map((jobSet: any, setIndex: number) => (
+                <div key={setIndex}>
+                    <Typography variant="h6" gutterBottom>
+                        Grupo {setIndex + 1}
+                    </Typography>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell className="white-text">ID</TableCell>
+                                <TableCell className="white-text">Descrição</TableCell> {/* Aplicando a classe aqui */}
+                                <TableCell className="white-text">Data Máxima de Conclusão</TableCell> {/* Aplicando a classe aqui */}
+                                <TableCell className="white-text">Tempo Estimado</TableCell> {/* Aplicando a classe aqui */}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {jobSet.map((job: any, jobIndex: number) => (
+                                <TableRow key={`${job.ID}-${setIndex}-${jobIndex}`}>
+                                    <TableCell>
+                                        <Typography variant="body1" component="p" className="white-text">
+                                            {job.ID}
+                                        </Typography>
+                                    </TableCell>
+                                    <TableCell className="white-text">{job.Descrição}</TableCell> {/* Aplicando a classe aqui */}
+                                    <TableCell className="white-text">{job['Data Máxima de conclusão']}</TableCell> {/* Aplicando a classe aqui */}
+                                    <TableCell className="white-text">{job['Tempo estimado']}</TableCell> {/* Aplicando a classe aqui */}
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </div>
+            ))}
+
 
         </>
     );
